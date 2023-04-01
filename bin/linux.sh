2f2230f6f20bbd7fccefef0b504c702d87202db3
Just999n <89408879+Just999n@users.noreply.github.com>
2023-04-01 11:18:12 +0800

    Created linux shell setup scripts.

---
+++ bin/linux.sh
@@ -0,0 +1,19 @@
+if [[ $(uname) != "Linux" ]]; then
+  echo "Error: This script only works on Linux systems." >&2
+  exit 1
+fi
+
+if [[ ! -d ~/TRASH ]]; then
+  mkdir ~/TRASH
+fi
+
+if [[ -f ~/.vimrc ]]; then
+  mv ~/.vimrc ~/.bupvimrc
+  echo "Your current vimrc file has been changed to .bupvimrc" >&2
+fi
+
+cp /etc/vimrc ~/.vimrc
+
+echo "source ~/dotfiles/etc/bashrc custom" >> ~/.bashrc
+
+exit 0
